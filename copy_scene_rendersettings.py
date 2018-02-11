a424e0e9b6e171b8ea5feec96b2fd0ec487dcbb6
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2018-02-11 03:36:54 +0000

    copy Cycles-related scene settings as well

--- copy_scene_rendersettings.py
+++ copy_scene_rendersettings.py
@@ -13,7 +13,7 @@ bl_info = \
     {
         "name" : "Copy Scene Render Settings",
         "author" : "Lawrence D'Oliveiro <ldo@geek-central.gen.nz>",
-        "version" : (0, 2, 0),
+        "version" : (0, 3, 0),
         "blender" : (2, 7, 9),
         "location" : "Properties → Render → Copy From Scene",
         "description" :
@@ -279,6 +279,106 @@ class CopySceneRenderAction(bpy.types.Operator) :
                 # collection of SceneRenderView
             "views_format" : None,
         }
+    cycles_render_props = \
+        { # Cycles-specific scene attributes
+            "cycles" :
+                { # CyclesRenderSettings fields
+                    "aa_samples" : None,
+                    "ao_bounces" : None,
+                    "ao_bounces_render" : None,
+                    "ao_samples" : None,
+                    "bake_type" : None,
+                    "blur_glossy" : None,
+                    "camera_cull_margin" : None,
+                    "caustics_reflective" : None,
+                    "caustics_refractive" : None,
+                    "debug_bvh_time_steps" : None,
+                    "debug_bvh_type" : None,
+                    "debug_cancel_timeout" : None,
+                    "debug_opencl_device_type" : None,
+                    "debug_opencl_kernel_single_program" : None,
+                    "debug_opencl_kernel_type" : None,
+                    "debug_opencl_mem_limit" : None,
+                    "debug_reset_timeout" : None,
+                    "debug_text_timeout" : None,
+                    "debug_tile_size" : None,
+                    "debug_use_cpu_avx" : None,
+                    "debug_use_cpu_avx2" : None,
+                    "debug_use_cpu_split_kernel" : None,
+                    "debug_use_cpu_sse2" : None,
+                    "debug_use_cpu_sse3" : None,
+                    "debug_use_cpu_sse41" : None,
+                    "debug_use_cuda_adaptive_compile" : None,
+                    "debug_use_cuda_split_kernel" : None,
+                    "debug_use_hair_bvh" : None,
+                    "debug_use_opencl_debug" : None,
+                    "debug_use_qbvh" : None,
+                    "debug_use_spatial_splits" : None,
+                    "device" : None,
+                    "dicing_rate" : None,
+                    "diffuse_bounces" : None,
+                    "diffuse_samples" : None,
+                    "distance_cull_margin" : None,
+                    "feature_set" : None,
+                    "film_exposure" : None,
+                    "film_transparent" : None,
+                    "filter_type" : None,
+                    "filter_width" : None,
+                    "glossy_bounces" : None,
+                    "glossy_samples" : None,
+                    "light_sampling_threshold" : None,
+                    "max_bounces" : None,
+                    "max_subdivisions" : None,
+                    "mesh_light_samples" : None,
+                    "motion_blur_position" : None,
+                    "pixel_filter_type" : None,
+                    "preview_aa_samples" : None,
+                    "preview_active_layer" : None,
+                    "preview_dicing_rate" : None,
+                    # "preview_pause" : None, # skip this?
+                    "preview_samples" : None,
+                    "preview_start_resolution" : None,
+                    "progressive" : None,
+                    "rolling_shutter_duration" : None,
+                    "rolling_shutter_type" : None,
+                    "sample_all_lights_direct" : None,
+                    "sample_all_lights_indirect" : None,
+                    "sample_clamp_direct" : None,
+                    "sample_clamp_indirect" : None,
+                    "samples" : None,
+                    "sampling_pattern" : None,
+                    "seed" : None,
+                    "shading_system" : None,
+                    "subsurface_samples" : None,
+                    "texture_limit" : None,
+                    "texture_limit_render" : None,
+                    "tile_order" : None,
+                    "transmission_bounces" : None,
+                    "transmission_samples" : None,
+                    "transparent_max_bounces" : None,
+                    "use_animated_seed" : None,
+                    "use_camera_cull" : None,
+                    "use_distance_cull" : None,
+                    "use_layer_samples" : None,
+                    "use_progressive_refine" : None,
+                    "use_square_samples" : None,
+                    "volume_bounces" : None,
+                    "volume_max_steps" : None,
+                    "volume_samples" : None,
+                    "volume_step_size" : None,
+                },
+            "cycles_curves" :
+                { # CyclesCurveRenderSettings fields
+                    "cull_backfacing" : None,
+                    "maximum_width" : None,
+                    "minimum_width" : None,
+                    "primitive" : None,
+                    "resolution" : None,
+                    "shape" : None,
+                    "subdivisions" : None,
+                    "use_curves" : None,
+                },
+        }
 
     def execute(self, context) :
 
@@ -317,13 +417,26 @@ class CopySceneRenderAction(bpy.types.Operator) :
     #begin execute
         from_scene_name = context.scene.copy_render_scene
         if from_scene_name != "" :
+            from_scene = bpy.data.scenes[from_scene_name]
+            to_scene = context.scene
             copy_attrs \
               (
                 root_name = "render",
-                src = bpy.data.scenes[from_scene_name].render,
-                dst = context.scene.render,
+                src = from_scene.render,
+                dst = to_scene.render,
                 attrs = self.render_props
               )
+            if from_scene.render.engine == "CYCLES" and to_scene.render.engine == "CYCLES" :
+                for props_name, props in self.cycles_render_props.items() :
+                    copy_attrs \
+                      (
+                        root_name = props_name,
+                        src = getattr(from_scene, props_name),
+                        dst = getattr(to_scene, props_name),
+                        attrs = props
+                      )
+                #end for
+            #end if
             status = {"FINISHED"}
         else :
             self.report({"ERROR"}, "No other scene")
